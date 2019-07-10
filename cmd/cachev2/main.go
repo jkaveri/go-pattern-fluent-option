f7244c0fde9da95cc72a2bdf6e2a3324b1d497c7
Ho Nguyen <haiho1992@gmail.com>
2019-07-10 20:58:12 +0700

    update code

--- cmd/cachev2/main.go
+++ cmd/cachev2/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/jkaveri/fluent-option/cachev2"
+import (
+	"time"
+
+	"github.com/jkaveri/fluent-option/cachev2"
+)
 
 func main() {
 	connection := "localhost"
 	password := "JA$2mAe%1@s5"
 	db := 0
+	maxRetries := 2
+	minRetryWithBackoff := 2 * time.Second
+	maxRetryWithBackoff := 2 * time.Second
 
-	redisCache := cachev2.NewRedisCache(connection, password, db)
+	redisCache := cachev2.NewRedisCache(
+		connection,
+		password,
+		db,
+		maxRetries,
+		minRetryWithBackoff,
+		maxRetryWithBackoff,
+	)
 
 	// set key
 	_ = redisCache.Set("my_key", "my_value")
